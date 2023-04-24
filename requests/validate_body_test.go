bc067652e6770910d276d78ed9ee238f3e896bc9
Dave Shanley <dave@quobix.com>
2023-04-24 10:31:53 -0400

    adding some test cases
    Signed-off-by: Dave Shanley <dave@quobix.com>

--- requests/validate_body_test.go
+++ requests/validate_body_test.go
@@ -148,6 +148,53 @@ paths:
 
 }
 
+func TestValidateBody_ContentTypeNotFound(t *testing.T) {
+    spec := `openapi: 3.1.0
+paths:
+  /burgers/createBurger:
+    post:
+      requestBody:
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                name:
+                  type: string
+                patties:
+                  type: integer
+                vegetarian:
+                  type: boolean`
+
+    doc, _ := libopenapi.NewDocument([]byte(spec))
+
+    m, _ := doc.BuildV3Model()
+    v := NewRequestBodyValidator(&m.Model)
+
+    // mix up the primitives to fire two schema violations.
+    body := map[string]interface{}{
+        "name":       "Big Mac",
+        "patties":    2,
+        "vegetarian": true,
+    }
+
+    bodyBytes, _ := json.Marshal(body)
+
+    request, _ := http.NewRequest(http.MethodPost, "https://things.com/burgers/createBurger",
+        bytes.NewBuffer(bodyBytes))
+    request.Header.Set("content-type", "application/not-json")
+
+    // preset the path
+    path, _, pv := paths.FindPath(request, &m.Model)
+    v.SetPathItem(path, pv)
+
+    valid, errors := v.ValidateRequestBody(request)
+
+    assert.False(t, valid)
+    assert.Len(t, errors, 1)
+
+}
+
 func TestValidateBody_InvalidBasicSchema(t *testing.T) {
     spec := `openapi: 3.1.0
 paths:
