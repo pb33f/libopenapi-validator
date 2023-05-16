576b3ff2975d88a766846ac21e80be1a7a397631
Dave Shanley <dave@quobix.com>
2023-05-16 11:55:38 -0400

    Added support for `jsonschema.InvalidJSONTypeError`
    Fixes a bug report filed in vacuum https://github.com/daveshanley/vacuum/issues/271
    
    Test confirms support.
    
    Excuse the formatting, I moved from windows to mac dev a month ago, and all my files are being cleaned automatically.
    
    Signed-off-by: Dave Shanley <dave@quobix.com>

--- schema_validation/validate_schema_test.go
+++ schema_validation/validate_schema_test.go
@@ -4,16 +4,16 @@
 package schema_validation
 
 import (
-    "encoding/json"
-    "github.com/pb33f/libopenapi"
-    "github.com/stretchr/testify/assert"
-    "gopkg.in/yaml.v3"
-    "testing"
+	"encoding/json"
+	"github.com/pb33f/libopenapi"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+	"testing"
 )
 
 func TestLocateSchemaPropertyNodeByJSONPath(t *testing.T) {
 
-    spec := `openapi: 3.1.0
+	spec := `openapi: 3.1.0
 paths:
   /burgers/createBurger:
     post:
@@ -30,23 +30,23 @@ paths:
                 vegetarian:
                   type: boolean`
 
-    var node yaml.Node
-    _ = yaml.Unmarshal([]byte(spec), &node)
+	var node yaml.Node
+	_ = yaml.Unmarshal([]byte(spec), &node)
 
-    foundNode := LocateSchemaPropertyNodeByJSONPath(node.Content[0],
-        "/paths/~1burgers~1createBurger/post/requestBody/content/application~1json/schema/properties/vegetarian")
+	foundNode := LocateSchemaPropertyNodeByJSONPath(node.Content[0],
+		"/paths/~1burgers~1createBurger/post/requestBody/content/application~1json/schema/properties/vegetarian")
 
-    assert.Equal(t, "boolean", foundNode.Content[1].Value)
+	assert.Equal(t, "boolean", foundNode.Content[1].Value)
 
-    foundNode = LocateSchemaPropertyNodeByJSONPath(node.Content[0],
-        "/i/do/not/exist")
+	foundNode = LocateSchemaPropertyNodeByJSONPath(node.Content[0],
+		"/i/do/not/exist")
 
-    assert.Nil(t, foundNode)
+	assert.Nil(t, foundNode)
 
 }
 
 func TestValidateSchema_SimpleValid_String(t *testing.T) {
-    spec := `openapi: 3.1.0
+	spec := `openapi: 3.1.0
 paths:
   /burgers/createBurger:
     post:
@@ -63,32 +63,32 @@ paths:
                 vegetarian:
                   type: boolean`
 
-    doc, _ := libopenapi.NewDocument([]byte(spec))
+	doc, _ := libopenapi.NewDocument([]byte(spec))
 
-    m, _ := doc.BuildV3Model()
+	m, _ := doc.BuildV3Model()
 
-    body := map[string]interface{}{
-        "name":       "Big Mac",
-        "patties":    2,
-        "vegetarian": true,
-    }
+	body := map[string]interface{}{
+		"name":       "Big Mac",
+		"patties":    2,
+		"vegetarian": true,
+	}
 
-    bodyBytes, _ := json.Marshal(body)
-    sch := m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
+	bodyBytes, _ := json.Marshal(body)
+	sch := m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
 
-    // create a schema validator
-    v := NewSchemaValidator()
+	// create a schema validator
+	v := NewSchemaValidator()
 
-    // validate!
-    valid, errors := v.ValidateSchemaString(sch.Schema(), string(bodyBytes))
+	// validate!
+	valid, errors := v.ValidateSchemaString(sch.Schema(), string(bodyBytes))
 
-    assert.True(t, valid)
-    assert.Len(t, errors, 0)
+	assert.True(t, valid)
+	assert.Len(t, errors, 0)
 
 }
 
 func TestValidateSchema_SimpleValid(t *testing.T) {
-    spec := `openapi: 3.1.0
+	spec := `openapi: 3.1.0
 paths:
   /burgers/createBurger:
     post:
@@ -105,32 +105,32 @@ paths:
                 vegetarian:
                   type: boolean`
 
-    doc, _ := libopenapi.NewDocument([]byte(spec))
+	doc, _ := libopenapi.NewDocument([]byte(spec))
 
-    m, _ := doc.BuildV3Model()
+	m, _ := doc.BuildV3Model()
 
-    body := map[string]interface{}{
-        "name":       "Big Mac",
-        "patties":    2,
-        "vegetarian": true,
-    }
+	body := map[string]interface{}{
+		"name":       "Big Mac",
+		"patties":    2,
+		"vegetarian": true,
+	}
 
-    // create a schema validator
-    v := NewSchemaValidator()
+	// create a schema validator
+	v := NewSchemaValidator()
 
-    bodyBytes, _ := json.Marshal(body)
-    sch := m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
+	bodyBytes, _ := json.Marshal(body)
+	sch := m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
 
-    // validate!
-    valid, errors := v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
+	// validate!
+	valid, errors := v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
 
-    assert.True(t, valid)
-    assert.Len(t, errors, 0)
+	assert.True(t, valid)
+	assert.Len(t, errors, 0)
 
 }
 
 func TestValidateSchema_SimpleInValid(t *testing.T) {
-    spec := `openapi: 3.1.0
+	spec := `openapi: 3.1.0
 paths:
   /burgers/createBurger:
     post:
@@ -147,32 +147,71 @@ paths:
                 vegetarian:
                   type: boolean`
 
-    doc, _ := libopenapi.NewDocument([]byte(spec))
+	doc, _ := libopenapi.NewDocument([]byte(spec))
 
-    m, _ := doc.BuildV3Model()
+	m, _ := doc.BuildV3Model()
 
-    body := map[string]interface{}{
-        "name":       "Big Mac",
-        "patties":    "I am not a number", // will fail
-        "vegetarian": 23,                  // will fail
-    }
+	body := map[string]interface{}{
+		"name":       "Big Mac",
+		"patties":    "I am not a number", // will fail
+		"vegetarian": 23,                  // will fail
+	}
 
-    // create a schema validator
-    v := NewSchemaValidator()
+	// create a schema validator
+	v := NewSchemaValidator()
 
-    bodyBytes, _ := json.Marshal(body)
-    sch := m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
+	bodyBytes, _ := json.Marshal(body)
+	sch := m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
 
-    // validate!
-    valid, errors := v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
+	// validate!
+	valid, errors := v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
 
-    assert.False(t, valid)
-    assert.Len(t, errors, 1)
-    assert.Len(t, errors[0].SchemaValidationErrors, 2)
+	assert.False(t, valid)
+	assert.Len(t, errors, 1)
+	assert.Len(t, errors[0].SchemaValidationErrors, 2)
+}
+
+func TestValidateSchema_InvalidJSONType(t *testing.T) {
+	spec := `openapi: 3.1.0
+paths:
+  /burgers/createBurger:
+    post:
+      requestBody:
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                name:
+                  type: string
+                patties:
+                  type: integer
+                vegetarian:
+                  type: boolean`
+
+	doc, _ := libopenapi.NewDocument([]byte(spec))
+
+	m, _ := doc.BuildV3Model()
+
+	body := struct{ name string }{"hello world"}
+
+	// create a schema validator
+	v := NewSchemaValidator()
+
+	//bodyBytes, _ := json.Marshal(body)
+	sch := m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
+
+	// validate!
+	valid, errors := v.ValidateSchemaObject(sch.Schema(), body)
+
+	assert.False(t, valid)
+	assert.Len(t, errors, 1)
+	assert.Len(t, errors[0].SchemaValidationErrors, 1)
+	assert.Equal(t, "jsonschema: invalid jsonType: struct { name string }", errors[0].SchemaValidationErrors[0].Reason)
 }
 
 func TestValidateSchema_ReffyComplex_Valid(t *testing.T) {
-    spec := `openapi: 3.1.0
+	spec := `openapi: 3.1.0
 components:
   schemas:
     Death:
@@ -224,57 +263,57 @@ paths:
             schema:
               $ref: '#/components/schemas/One'`
 
-    doc, _ := libopenapi.NewDocument([]byte(spec))
+	doc, _ := libopenapi.NewDocument([]byte(spec))
 
-    m, _ := doc.BuildV3Model()
+	m, _ := doc.BuildV3Model()
 
-    cakePlease := map[string]interface{}{
-        "two": map[string]interface{}{
-            "three": map[string]interface{}{
-                "four": map[string]interface{}{
-                    "cakeOrDeath": "cake please",
-                },
-            },
-        },
-    }
+	cakePlease := map[string]interface{}{
+		"two": map[string]interface{}{
+			"three": map[string]interface{}{
+				"four": map[string]interface{}{
+					"cakeOrDeath": "cake please",
+				},
+			},
+		},
+	}
 
-    death := map[string]interface{}{
-        "two": map[string]interface{}{
-            "three": map[string]interface{}{
-                "four": map[string]interface{}{
-                    "cakeOrDeath": "death",
-                },
-            },
-        },
-    }
+	death := map[string]interface{}{
+		"two": map[string]interface{}{
+			"three": map[string]interface{}{
+				"four": map[string]interface{}{
+					"cakeOrDeath": "death",
+				},
+			},
+		},
+	}
 
-    // cake? (https://www.youtube.com/watch?v=PVH0gZO5lq0)
-    bodyBytes, _ := json.Marshal(cakePlease)
-    sch := m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
+	// cake? (https://www.youtube.com/watch?v=PVH0gZO5lq0)
+	bodyBytes, _ := json.Marshal(cakePlease)
+	sch := m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
 
-    // create a schema validator
-    v := NewSchemaValidator()
+	// create a schema validator
+	v := NewSchemaValidator()
 
-    // validate!
-    valid, errors := v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
+	// validate!
+	valid, errors := v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
 
-    assert.True(t, valid)
-    assert.Len(t, errors, 0)
+	assert.True(t, valid)
+	assert.Len(t, errors, 0)
 
-    // or death!
-    bodyBytes, _ = json.Marshal(death)
-    sch = m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
+	// or death!
+	bodyBytes, _ = json.Marshal(death)
+	sch = m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
 
-    // validate!
-    valid, errors = v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
+	// validate!
+	valid, errors = v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
 
-    assert.True(t, valid)
-    assert.Len(t, errors, 0)
+	assert.True(t, valid)
+	assert.Len(t, errors, 0)
 
 }
 
 func TestValidateSchema_ReffyComplex_Invalid(t *testing.T) {
-    spec := `openapi: 3.1.0
+	spec := `openapi: 3.1.0
 components:
   schemas:
     Death:
@@ -326,65 +365,65 @@ paths:
             schema:
               $ref: '#/components/schemas/One'`
 
-    doc, _ := libopenapi.NewDocument([]byte(spec))
+	doc, _ := libopenapi.NewDocument([]byte(spec))
 
-    m, _ := doc.BuildV3Model()
+	m, _ := doc.BuildV3Model()
 
-    cakePlease := map[string]interface{}{
-        "two": map[string]interface{}{
-            "three": map[string]interface{}{
-                "four": map[string]interface{}{
-                    "cakeOrDeath": "no more cake? so the choice is 'or death?'",
-                },
-            },
-        },
-    }
+	cakePlease := map[string]interface{}{
+		"two": map[string]interface{}{
+			"three": map[string]interface{}{
+				"four": map[string]interface{}{
+					"cakeOrDeath": "no more cake? so the choice is 'or death?'",
+				},
+			},
+		},
+	}
 
-    death := map[string]interface{}{
-        "two": map[string]interface{}{
-            "three": map[string]interface{}{
-                "four": map[string]interface{}{
-                    "cakeOrDeath": "i'll have the chicken",
-                },
-            },
-        },
-    }
+	death := map[string]interface{}{
+		"two": map[string]interface{}{
+			"three": map[string]interface{}{
+				"four": map[string]interface{}{
+					"cakeOrDeath": "i'll have the chicken",
+				},
+			},
+		},
+	}
 
-    // cake? (https://www.youtube.com/watch?v=PVH0gZO5lq0)
-    bodyBytes, _ := json.Marshal(cakePlease)
-    sch := m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
+	// cake? (https://www.youtube.com/watch?v=PVH0gZO5lq0)
+	bodyBytes, _ := json.Marshal(cakePlease)
+	sch := m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
 
-    // create a schema validator
-    v := NewSchemaValidator()
+	// create a schema validator
+	v := NewSchemaValidator()
 
-    // validate!
-    valid, errors := v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
+	// validate!
+	valid, errors := v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
 
-    assert.False(t, valid)
-    assert.Len(t, errors, 1)
-    assert.Len(t, errors[0].SchemaValidationErrors, 3)
+	assert.False(t, valid)
+	assert.Len(t, errors, 1)
+	assert.Len(t, errors[0].SchemaValidationErrors, 3)
 
-    valid, errors = v.ValidateSchemaObject(sch.Schema(), cakePlease)
+	valid, errors = v.ValidateSchemaObject(sch.Schema(), cakePlease)
 
-    assert.False(t, valid)
-    assert.Len(t, errors, 1)
-    assert.Len(t, errors[0].SchemaValidationErrors, 3)
+	assert.False(t, valid)
+	assert.Len(t, errors, 1)
+	assert.Len(t, errors[0].SchemaValidationErrors, 3)
 
-    // or death!
-    bodyBytes, _ = json.Marshal(death)
-    sch = m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
+	// or death!
+	bodyBytes, _ = json.Marshal(death)
+	sch = m.Model.Paths.PathItems["/burgers/createBurger"].Post.RequestBody.Content["application/json"].Schema
 
-    // validate!
-    valid, errors = v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
+	// validate!
+	valid, errors = v.ValidateSchemaBytes(sch.Schema(), bodyBytes)
 
-    assert.False(t, valid)
-    assert.Len(t, errors, 1)
-    assert.Len(t, errors[0].SchemaValidationErrors, 3)
+	assert.False(t, valid)
+	assert.Len(t, errors, 1)
+	assert.Len(t, errors[0].SchemaValidationErrors, 3)
 
-    valid, errors = v.ValidateSchemaObject(sch.Schema(), death)
+	valid, errors = v.ValidateSchemaObject(sch.Schema(), death)
 
-    assert.False(t, valid)
-    assert.Len(t, errors, 1)
-    assert.Len(t, errors[0].SchemaValidationErrors, 3)
+	assert.False(t, valid)
+	assert.Len(t, errors, 1)
+	assert.Len(t, errors[0].SchemaValidationErrors, 3)
 
 }
