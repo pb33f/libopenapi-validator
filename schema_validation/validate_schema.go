694596fd8fcf9247291cfdcfbea97d5ca4b43e49
Dave Shanley <dave@quobix.com>
2023-10-08 12:32:40 -0400

    Updated support for most of usescases in #26
    bumped `libopenapi` to `v0.12.1`
    
    Signed-off-by: Dave Shanley <dave@quobix.com>

--- schema_validation/validate_schema.go
+++ schema_validation/validate_schema.go
@@ -4,9 +4,11 @@
 package schema_validation
 
 import (
+	_ "embed"
 	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/pb33f/libopenapi-validator/errors"
+	liberrors "github.com/pb33f/libopenapi-validator/errors"
 	"github.com/pb33f/libopenapi-validator/helpers"
 	"github.com/pb33f/libopenapi/datamodel/high/base"
 	"github.com/pb33f/libopenapi/utils"
@@ -30,15 +32,15 @@ import (
 type SchemaValidator interface {
 
 	// ValidateSchemaString accepts a schema object to validate against, and a JSON/YAML blob that is defined as a string.
-	ValidateSchemaString(schema *base.Schema, payload string) (bool, []*errors.ValidationError)
+	ValidateSchemaString(schema *base.Schema, payload string) (bool, []*liberrors.ValidationError)
 
 	// ValidateSchemaObject accepts a schema object to validate against, and an object, created from unmarshalled JSON/YAML.
 	// This is a pre-decoded object that will skip the need to unmarshal a string of JSON/YAML.
-	ValidateSchemaObject(schema *base.Schema, payload interface{}) (bool, []*errors.ValidationError)
+	ValidateSchemaObject(schema *base.Schema, payload interface{}) (bool, []*liberrors.ValidationError)
 
 	// ValidateSchemaBytes accepts a schema object to validate against, and a byte slice containing a schema to
 	// validate against.
-	ValidateSchemaBytes(schema *base.Schema, payload []byte) (bool, []*errors.ValidationError)
+	ValidateSchemaBytes(schema *base.Schema, payload []byte) (bool, []*liberrors.ValidationError)
 }
 
 var instanceLocationRegex = regexp.MustCompile(`^/(\d+)`)
@@ -53,33 +55,41 @@ func NewSchemaValidator() SchemaValidator {
 	return &schemaValidator{logger: logger.Sugar()}
 }
 
-func (s *schemaValidator) ValidateSchemaString(schema *base.Schema, payload string) (bool, []*errors.ValidationError) {
+func (s *schemaValidator) ValidateSchemaString(schema *base.Schema, payload string) (bool, []*liberrors.ValidationError) {
 	return validateSchema(schema, []byte(payload), nil, s.logger)
 }
 
-func (s *schemaValidator) ValidateSchemaObject(schema *base.Schema, payload interface{}) (bool, []*errors.ValidationError) {
+func (s *schemaValidator) ValidateSchemaObject(schema *base.Schema, payload interface{}) (bool, []*liberrors.ValidationError) {
 	return validateSchema(schema, nil, payload, s.logger)
 }
 
-func (s *schemaValidator) ValidateSchemaBytes(schema *base.Schema, payload []byte) (bool, []*errors.ValidationError) {
+func (s *schemaValidator) ValidateSchemaBytes(schema *base.Schema, payload []byte) (bool, []*liberrors.ValidationError) {
 	return validateSchema(schema, payload, nil, s.logger)
 }
 
 var renderLock = &sync.Mutex{}
 
-func validateSchema(schema *base.Schema, payload []byte, decodedObject interface{}, log *zap.SugaredLogger) (bool, []*errors.ValidationError) {
+func validateSchema(schema *base.Schema, payload []byte, decodedObject interface{}, log *zap.SugaredLogger) (bool, []*liberrors.ValidationError) {
 
-	var validationErrors []*errors.ValidationError
+	var validationErrors []*liberrors.ValidationError
 
 	if schema == nil {
 		log.Infoln("schema is empty and cannot be validated. This generally means the schema is missing from the spec, or could not be read.")
 		return false, validationErrors
 	}
 
+	// extract index of schema, and check the version
+	schemaIndex := schema.GoLow().Index
+	var renderedSchema []byte
+
 	// render the schema, to be used for validation, stop this from running concurrently, mutations are made to state
 	// and, it will cause async issues.
 	renderLock.Lock()
-	renderedSchema, _ := schema.RenderInline()
+	version := float32(0.0)
+	if schemaIndex != nil {
+		version = schemaIndex.GetConfig().SpecInfo.VersionNumeric
+		renderedSchema, _ = schema.RenderInline()
+	}
 	renderLock.Unlock()
 
 	jsonSchema, _ := utils.ConvertYAMLtoJSON(renderedSchema)
@@ -89,21 +99,21 @@ func validateSchema(schema *base.Schema, payload []byte, decodedObject interface
 
 		if err != nil {
 			// cannot decode the request body, so it's not valid
-			violation := &errors.SchemaValidationFailure{
+			violation := &liberrors.SchemaValidationFailure{
 				Reason:          err.Error(),
 				Location:        "unavailable",
 				ReferenceSchema: string(renderedSchema),
 				ReferenceObject: string(payload),
 			}
-			validationErrors = append(validationErrors, &errors.ValidationError{
+			validationErrors = append(validationErrors, &liberrors.ValidationError{
 				ValidationType:         helpers.RequestBodyValidation,
 				ValidationSubType:      helpers.Schema,
 				Message:                "schema does not pass validation",
 				Reason:                 fmt.Sprintf("The schema cannot be decoded: %s", err.Error()),
 				SpecLine:               1,
 				SpecCol:                0,
-				SchemaValidationErrors: []*errors.SchemaValidationFailure{violation},
-				HowToFix:               errors.HowToFixInvalidSchema,
+				SchemaValidationErrors: []*liberrors.SchemaValidationFailure{violation},
+				HowToFix:               liberrors.HowToFixInvalidSchema,
 				Context:                string(renderedSchema), // attach the rendered schema to the error
 			})
 			return false, validationErrors
@@ -111,86 +121,72 @@ func validateSchema(schema *base.Schema, payload []byte, decodedObject interface
 
 	}
 	compiler := jsonschema.NewCompiler()
+	if version >= 3.1 {
+		compiler.Draft = jsonschema.Draft2020
+	} else {
+		compiler.Draft = jsonschema.Draft4
+	}
+
 	_ = compiler.AddResource("schema.json", strings.NewReader(string(jsonSchema)))
-	jsch, _ := compiler.Compile("schema.json")
+	jsch, err := compiler.Compile("schema.json")
+
+	var schemaValidationErrors []*liberrors.SchemaValidationFailure
+
+	// is the schema even valid? did it compile?
+	if err != nil {
+		var se *jsonschema.SchemaError
+		if errors.As(err, &se) {
+			var ve *jsonschema.ValidationError
+			if errors.As(se.Err, &ve) {
+
+				// no, this won't work, so we need to extract the errors and return them.
+				basicErrors := ve.BasicOutput().Errors
+				schemaValidationErrors = extractBasicErrors(basicErrors, renderedSchema, decodedObject, payload, ve, schemaValidationErrors)
+				// cannot compile schema, so it's not valid
+				violation := &liberrors.SchemaValidationFailure{
+					Reason:          err.Error(),
+					Location:        "unavailable",
+					ReferenceSchema: string(renderedSchema),
+					ReferenceObject: string(payload),
+				}
+				validationErrors = append(validationErrors, &liberrors.ValidationError{
+					ValidationType:         helpers.RequestBodyValidation,
+					ValidationSubType:      helpers.Schema,
+					Message:                "schema does not pass validation",
+					Reason:                 fmt.Sprintf("The schema cannot be decoded: %s", err.Error()),
+					SpecLine:               1,
+					SpecCol:                0,
+					SchemaValidationErrors: []*liberrors.SchemaValidationFailure{violation},
+					HowToFix:               liberrors.HowToFixInvalidSchema,
+					Context:                string(renderedSchema), // attach the rendered schema to the error
+				})
+				return false, validationErrors
+			}
+		}
+	}
 
 	// 4. validate the object against the schema
-	if decodedObject != nil {
+	if jsch != nil && decodedObject != nil {
 		scErrs := jsch.Validate(decodedObject)
 		if scErrs != nil {
-			var schemaValidationErrors []*errors.SchemaValidationFailure
 
 			// check for invalid JSON type errors.
-			if _, ok := scErrs.(jsonschema.InvalidJSONTypeError); ok {
-				violation := &errors.SchemaValidationFailure{
+			var invalidJSONTypeError jsonschema.InvalidJSONTypeError
+			if errors.As(scErrs, &invalidJSONTypeError) {
+				violation := &liberrors.SchemaValidationFailure{
 					Reason:   scErrs.Error(),
 					Location: "unavailable", // we don't have a location for this error, so we'll just say it's unavailable.
 				}
 				schemaValidationErrors = append(schemaValidationErrors, violation)
 			}
 
-			if jk, ok := scErrs.(*jsonschema.ValidationError); ok {
+			var jk *jsonschema.ValidationError
+			if errors.As(scErrs, &jk) {
 
 				// flatten the validationErrors
 				schFlatErrs := jk.BasicOutput().Errors
 
-				for q := range schFlatErrs {
-					er := schFlatErrs[q]
-					if er.KeywordLocation == "" || strings.HasPrefix(er.Error, "doesn't validate with") {
-						continue // ignore this error, it's useless tbh, utter noise.
-					}
-					if er.Error != "" {
-
-						// re-encode the schema.
-						var renderedNode yaml.Node
-						_ = yaml.Unmarshal(renderedSchema, &renderedNode)
-
-						// locate the violated property in the schema
-						located := LocateSchemaPropertyNodeByJSONPath(renderedNode.Content[0], er.KeywordLocation)
-
-						// extract the element specified by the instance
-						val := instanceLocationRegex.FindStringSubmatch(er.InstanceLocation)
-						var referenceObject string
-
-						if len(val) > 0 {
-							referenceIndex, _ := strconv.Atoi(val[1])
-							if reflect.ValueOf(decodedObject).Type().Kind() == reflect.Slice {
-								found := decodedObject.([]any)[referenceIndex]
-								recoded, _ := json.MarshalIndent(found, "", "  ")
-								referenceObject = string(recoded)
-							}
-						}
-						if referenceObject == "" {
-							referenceObject = string(payload)
-						}
-
-						violation := &errors.SchemaValidationFailure{
-							Reason:           er.Error,
-							Location:         er.InstanceLocation,
-							DeepLocation:     er.KeywordLocation,
-							AbsoluteLocation: er.AbsoluteKeywordLocation,
-							ReferenceSchema:  string(renderedSchema),
-							ReferenceObject:  referenceObject,
-							OriginalError:    jk,
-						}
-						// if we have a location within the schema, add it to the error
-						if located != nil {
-							line := located.Line
-							// if the located node is a map or an array, then the actual human interpretable
-							// line on which the violation occurred is the line of the key, not the value.
-							if located.Kind == yaml.MappingNode || located.Kind == yaml.SequenceNode {
-								if line > 0 {
-									line--
-								}
-							}
-
-							// location of the violation within the rendered schema.
-							violation.Line = line
-							violation.Column = located.Column
-						}
-						schemaValidationErrors = append(schemaValidationErrors, violation)
-					}
-				}
+				schemaValidationErrors = extractBasicErrors(schFlatErrs, renderedSchema, decodedObject, payload, jk, schemaValidationErrors)
 			}
 			line := 1
 			col := 0
@@ -200,14 +196,14 @@ func validateSchema(schema *base.Schema, payload []byte, decodedObject interface
 			}
 
 			// add the error to the list
-			validationErrors = append(validationErrors, &errors.ValidationError{
+			validationErrors = append(validationErrors, &liberrors.ValidationError{
 				ValidationType:         helpers.Schema,
 				Message:                "schema does not pass validation",
 				Reason:                 "Schema failed to validate against the contract requirements",
 				SpecLine:               line,
 				SpecCol:                col,
 				SchemaValidationErrors: schemaValidationErrors,
-				HowToFix:               errors.HowToFixInvalidSchema,
+				HowToFix:               liberrors.HowToFixInvalidSchema,
 				Context:                string(renderedSchema), // attach the rendered schema to the error
 			})
 		}
@@ -216,4 +212,68 @@ func validateSchema(schema *base.Schema, payload []byte, decodedObject interface
 		return false, validationErrors
 	}
 	return true, nil
+}
+
+func extractBasicErrors(schFlatErrs []jsonschema.BasicError,
+	renderedSchema []byte, decodedObject interface{},
+	payload []byte, jk *jsonschema.ValidationError,
+	schemaValidationErrors []*liberrors.SchemaValidationFailure) []*liberrors.SchemaValidationFailure {
+	for q := range schFlatErrs {
+		er := schFlatErrs[q]
+		if er.KeywordLocation == "" || strings.HasPrefix(er.Error, "doesn't validate with") {
+			continue // ignore this error, it's useless tbh, utter noise.
+		}
+		if er.Error != "" {
+
+			// re-encode the schema.
+			var renderedNode yaml.Node
+			_ = yaml.Unmarshal(renderedSchema, &renderedNode)
+
+			// locate the violated property in the schema
+			located := LocateSchemaPropertyNodeByJSONPath(renderedNode.Content[0], er.KeywordLocation)
+
+			// extract the element specified by the instance
+			val := instanceLocationRegex.FindStringSubmatch(er.InstanceLocation)
+			var referenceObject string
+
+			if len(val) > 0 {
+				referenceIndex, _ := strconv.Atoi(val[1])
+				if reflect.ValueOf(decodedObject).Type().Kind() == reflect.Slice {
+					found := decodedObject.([]any)[referenceIndex]
+					recoded, _ := json.MarshalIndent(found, "", "  ")
+					referenceObject = string(recoded)
+				}
+			}
+			if referenceObject == "" {
+				referenceObject = string(payload)
+			}
+
+			violation := &liberrors.SchemaValidationFailure{
+				Reason:           er.Error,
+				Location:         er.InstanceLocation,
+				DeepLocation:     er.KeywordLocation,
+				AbsoluteLocation: er.AbsoluteKeywordLocation,
+				ReferenceSchema:  string(renderedSchema),
+				ReferenceObject:  referenceObject,
+				OriginalError:    jk,
+			}
+			// if we have a location within the schema, add it to the error
+			if located != nil {
+				line := located.Line
+				// if the located node is a map or an array, then the actual human interpretable
+				// line on which the violation occurred is the line of the key, not the value.
+				if located.Kind == yaml.MappingNode || located.Kind == yaml.SequenceNode {
+					if line > 0 {
+						line--
+					}
+				}
+
+				// location of the violation within the rendered schema.
+				violation.Line = line
+				violation.Column = located.Column
+			}
+			schemaValidationErrors = append(schemaValidationErrors, violation)
+		}
+	}
+	return schemaValidationErrors
 }
