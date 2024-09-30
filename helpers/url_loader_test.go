489723fbe8a9e85355077f2e291763b5e0d0935e
Dave Shanley <dave@quobix.com>
2024-09-30 11:44:12 -0400

    whoops

--- helpers/url_loader_test.go
+++ helpers/url_loader_test.go
@@ -1,7 +1,8 @@
-//// Copyright 2023-2024 Princess Beef Heavy Industries, LLC / Dave Shanley
-//// https://pb33f.io
-//
-//package helpers
+// Copyright 2023-2024 Princess Beef Heavy Industries, LLC / Dave Shanley
+// https://pb33f.io
+
+package helpers
+
 //
 //import (
 //	"fmt"
