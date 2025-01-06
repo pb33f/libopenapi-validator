f1998a1b5c6c98b1cda302f68da1df18a9bee032
Jem Day <Jem.Day@cliffhanger.com>
2025-01-06 11:05:49 -0800

    - Increase test coverage.

--- helpers/schema_compiler_test.go
+++ helpers/schema_compiler_test.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/pb33f/libopenapi-validator/config"
@@ -60,4 +61,53 @@ func Test_ObjectSchema(t *testing.T) {
 
 	require.Nil(t, err, "Failed to compile Schema: %v", err)
 	require.NotNil(t, jsch, "Did not return a compiled schema")
+}
+
+func Test_ValidJSONSchemaWithInvalidContent(t *testing.T) {
+	// An example of a dubious JSON Schema
+	const badSchema = `{
+  "type": "you-dont-know-me",
+  "format": "date",
+  "minLength": 10
+}`
+
+	jsch, err := NewCompiledSchema("test", []byte(badSchema), nil)
+
+	assert.NotNil(t, err, "Expected an error to be thrown")
+	assert.Nil(t, jsch, "invalid schema compiled!")
+}
+
+func Test_MalformedSONSchema(t *testing.T) {
+	// An example of a JSON schema with malformed JSON
+	const badSchema = `{
+  "type": "you-dont-know-me",
+  "format": "date"
+  "minLength": 10
+}`
+
+	jsch, err := NewCompiledSchema("test", []byte(badSchema), nil)
+
+	assert.NotNil(t, err, "Expected an error to be thrown")
+	assert.Nil(t, jsch, "invalid schema compiled!")
+}
+
+func Test_ValidJSONSchemaWithIncompleteContent(t *testing.T) {
+	// An example of a dJSON schema with references to non-existent stuff
+	const incompleteSchema = `{
+  "type": "object",
+  "title" : "unresolvable",
+  "properties" : {
+     "name" : {
+	    "type": "string",
+     },
+     "species" : {
+      "$ref": "#/$defs/speciesEnum"
+     }
+  }
+}`
+
+	jsch, err := NewCompiledSchema("test", []byte(incompleteSchema), nil)
+
+	assert.NotNil(t, err, "Expected an error to be thrown")
+	assert.Nil(t, jsch, "invalid schema compiled!")
 }
